24ccc1b9933b70ac6c34a76bca6ee3fb61595d7a
SharkDucNA <95276179+AnhducNA@users.noreply.github.com>
2022-07-28 15:14:27 +0700

    shop

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    function listCat()
+    {
+        return view('product.cat.list');
+    }
+    function showDetail($id)
+    {
+        return view('product.detail.show');
+    }
 }
